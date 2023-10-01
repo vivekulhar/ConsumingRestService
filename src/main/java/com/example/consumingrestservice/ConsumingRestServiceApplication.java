7ab26ede3ff4ef910f89a7d3c28b5c63ee9c5de9
Vivek Kulhar <vivek.kulhar23@gmail.com>
2023-10-01 17:23:25 +0530

    Consume a Rest API

---
+++ src/main/java/com/example/consumingrestservice/ConsumingRestServiceApplication.java
@@ -0,0 +1,35 @@
+package com.example.consumingrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+@SpringBootApplication
+public class ConsumingRestServiceApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(ConsumingRestServiceApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingRestServiceApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Quote quote = restTemplate.getForObject(
+					"http://localhost:8080/api/random", Quote.class);
+			log.info(quote.toString());
+		};
+	}
+}
---
+++ src/test/java/com/example/consumingrestservice/ConsumingRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consumingrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
